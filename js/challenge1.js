5abd8912c2556c65bd77eade71bee245fb0a5f22
wenxiwaang <156857298+wenxiwaang@users.noreply.github.com>
2024-04-09 21:46:02 -0400

    Update challenge1.js

--- js/challenge1.js
+++ js/challenge1.js
@@ -1,2 +1,19 @@
 let style = document.querySelector('h1');
 style.classList.add('heading');
+
+document.addEventListener('dblclick', function() {
+    const currentTime = new Date();
+    alert(currentTime);
+});
+
+
+const checkbox = document.querySelector('#toggle');
+const emailBox = document.querySelector('#emailBox');
+
+checkbox.addEventListener('click', function() {
+    if (checkbox.checked) {
+        emailBox.classList.remove('hidden');
+    } else {
+        emailBox.classList.add('hidden');
+    }
+});
